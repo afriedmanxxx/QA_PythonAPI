dd065d53c92ab871f122307dfbb84f47c585bba2
afriedmanxxx <afriedmanxxx@gmail.com>
2022-03-13 13:44:31 -0700

    ex11

---
+++ excersices/ex11.py
@@ -0,0 +1,19 @@
+import requests
+import pytest
+
+'''
+Необходимо написать тест, который делает запрос на метод: https://playground.learnqa.ru/api/homework_cookie
+Этот метод возвращает какую-то cookie с каким-то значением. Необходимо с помощью функции print() понять что 
+за cookie и с каким значением, и зафиксировать это поведение с помощью assert
+Чтобы pytest не игнорировал print() необходимо использовать ключик "-s": python -m pytest -s my_test.py 
+'''
+
+
+def test_cookie():
+    response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
+    print(response.cookies.values())
+    print(response.cookies.keys())
+    print(response.cookies)
+
+    assert 'HomeWork ' in response.cookies, f"The key 'HomeWork' is not in {response.cookies} "
+
